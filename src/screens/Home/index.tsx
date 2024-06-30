de76d18d0e3fc438984b10bcee64aa69a659d571
Savio Anjos <savioanjos2020@gmail.com>
2024-06-29 21:09:39 -0300

    feat: :sparkles: Added TextInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
@@ -7,6 +7,12 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
       <StatusBar style="auto" />
     </View>
   );
